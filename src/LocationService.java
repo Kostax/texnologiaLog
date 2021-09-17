4e1d0ed3f6071db6fb7411c0244d66f933cfe2b0
Kostax <kostax.98@hotmail.com>
2021-09-17 19:03:41 +0300

    Initial commit

---
+++ src/LocationService.java
@@ -0,0 +1,19 @@
+public class LocationService {
+   private String longitude;
+   private String latitude;
+
+
+    public LocationService(String longitude, String latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+}
